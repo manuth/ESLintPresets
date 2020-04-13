374f78f3fd7140941ef5030a0562ce76f856f06b
Manuel Thalmann <m@nuth.ch>
2020-04-13 03:10:28 +0200

    Refactor the member-ordering config

--- src/Configuration/GenerateConfiguration.ts
+++ src/Configuration/GenerateConfiguration.ts
@@ -81,7 +81,68 @@ export function GenerateConfiguration(weak: boolean, typeChecking: boolean): any
                         }
                     }
                 ],
-                "@typescript-eslint/member-ordering": weak ? "off" : "warn",
+                "@typescript-eslint/member-ordering": [
+                    weak ? "off" : "warn",
+                    {
+                        default: [
+                            // Fields
+                            "public-static-field",
+                            "protected-static-field",
+                            "private-static-field",
+
+                            "public-instance-field",
+                            "protected-instance-field",
+                            "private-instance-field",
+
+                            "public-abstract-field",
+                            "protected-abstract-field",
+                            "private-abstract-field",
+
+                            "public-field",
+                            "protected-field",
+                            "private-field",
+
+                            "static-field",
+                            "instance-field",
+                            "abstract-field",
+
+                            "field",
+
+                            // Constructors
+                            "public-constructor",
+                            "protected-constructor",
+                            "private-constructor",
+
+                            "constructor",
+
+                            // Index signature
+                            "signature",
+
+                            // Methods
+                            "public-static-method",
+                            "protected-static-method",
+                            "private-static-method",
+
+                            "public-instance-method",
+                            "protected-instance-method",
+                            "private-instance-method",
+
+                            "public-abstract-method",
+                            "protected-abstract-method",
+                            "private-abstract-method",
+
+                            "public-method",
+                            "protected-method",
+                            "private-method",
+
+                            "static-method",
+                            "instance-method",
+                            "abstract-method",
+
+                            "method"
+                        ]
+                    }
+                ],
                 "@typescript-eslint/naming-convention": [
                     weak ? "off" : "warn",
                     {
