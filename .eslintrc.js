f98711dec6b552fc07348a3d567fd23f91f06fce
Manuel Thalmann <m@nuth.ch>
2020-04-17 20:29:22 +0200

    Enable es6 for this project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
     parser: "@typescript-eslint/parser",
+    env: {
+        es6: true
+    },
     parserOptions: {
+        sourceType: "module",
         project: "tsconfig.json"
     },
     extends: [
