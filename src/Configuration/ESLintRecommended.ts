fd406e2f8bce06bb06f7bd69c7b33d805c1e8a67
Manuel Thalmann <m@nuth.ch>
2020-04-17 20:28:01 +0200

    Disable the ts-overrides for js-files

--- src/Configuration/ESLintRecommended.ts
+++ src/Configuration/ESLintRecommended.ts
@@ -2,10 +2,4 @@ import merge = require("lodash.merge");
 const esLintRecommended: any = require("@typescript-eslint/eslint-plugin").configs["eslint-recommended"];
 
 let ESLintRecommended = merge({}, esLintRecommended);
-
-for (let override of ESLintRecommended.overrides)
-{
-    override.files.push("*.js", "*.jsx");
-}
-
 export = ESLintRecommended;
