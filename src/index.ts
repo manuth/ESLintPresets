c704c6dcc9033094b6d16ddcd333d3839266bfd1
Manuel Thalmann <m@nuth.ch>
2020-05-27 13:32:00 +0200

    Export the contributed configurations

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
-export = async () =>
-{
-    console.log("Hello World");
+import Recommended = require("./Configuration/Recommended");
+import RecommendedWithTypeChecking = require("./Configuration/RecommendedWithTypeChecking");
+import Weak = require("./Configuration/Weak");
+import WeakWithTypeChecking = require("./Configuration/WeakWithTypeChecking");
+
+export = {
+    configs: {
+        recommended: Recommended,
+        "recommended-requiring-type-checking": RecommendedWithTypeChecking,
+        weak: Weak,
+        "weak-requiring-type-checking": WeakWithTypeChecking
+    }
 };
