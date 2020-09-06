2a891cf8a1090b1f4b55188a0e8a4a528b209de6
Manuel Thalmann <m@nuth.ch>
2020-09-06 23:19:39 +0200

    Refactor the member-ordering rules

--- src/Configuration/GenerateConfiguration.ts
+++ src/Configuration/GenerateConfiguration.ts
@@ -89,60 +89,39 @@ export function GenerateConfiguration(weak: boolean, typeChecking: boolean): any
                     {
                         default: [
                             // Fields
-                            "public-static-field",
-                            "protected-static-field",
-                            "private-static-field",
-
-                            "public-instance-field",
-                            "protected-instance-field",
-                            "private-instance-field",
-
-                            "public-abstract-field",
-                            "protected-abstract-field",
-                            "private-abstract-field",
-
-                            "public-field",
-                            "protected-field",
-                            "private-field",
-
                             "static-field",
-                            "instance-field",
-                            "abstract-field",
-
                             "field",
 
+                            "public-static-field",
+                            "public-field",
+
+                            "protected-static-field",
+                            "protected-field",
+
+                            "private-static-field",
+                            "private-field",
+
                             // Constructors
+                            "constructor",
                             "public-constructor",
                             "protected-constructor",
                             "private-constructor",
-
-                            "constructor",
 
                             // Index signature
                             "signature",
 
                             // Methods
-                            "public-static-method",
-                            "protected-static-method",
-                            "private-static-method",
-
-                            "public-instance-method",
-                            "protected-instance-method",
-                            "private-instance-method",
-
-                            "public-abstract-method",
-                            "protected-abstract-method",
-                            "private-abstract-method",
-
-                            "public-method",
-                            "protected-method",
-                            "private-method",
-
                             "static-method",
-                            "instance-method",
-                            "abstract-method",
+                            "method",
 
-                            "method"
+                            "public-static-method",
+                            "public-method",
+
+                            "protected-static-method",
+                            "protected-method",
+
+                            "private-static-method",
+                            "private-method"
                         ]
                     }
                 ],
