0176fefa25d2ddc9817efae09fcea3b1daf455a4
Manuel Thalmann <m@nuth.ch>
2021-06-01 13:17:15 +0200

    Rename test-files

--- src/tests/RuleSets/Rules/Deprecation/index.ts
+++ src/tests/RuleSets/Rules/Deprecation/index.ts
@@ -1,5 +1,5 @@
 import { ContainerSuite } from "../../../Debugging/Suites/ContainerSuite";
-import { Deprecation } from "./Deprecation";
+import { Deprecation } from "./Deprecation.test";
 
 export let DeprecationTests = new ContainerSuite(
     "Deprecation",
--- src/tests/RuleSets/Rules/JSDoc/index.ts
+++ src/tests/RuleSets/Rules/JSDoc/index.ts
@@ -1,20 +1,20 @@
 import { ContainerSuite } from "../../../Debugging/Suites/ContainerSuite";
-import { CheckAlignment } from "./CheckAlignment";
-import { CheckIndentation } from "./CheckIndentation";
-import { CheckParamNames } from "./CheckParamNames";
-import { CheckSyntax } from "./CheckSyntax";
-import { CheckTagNames } from "./CheckTagNames";
-import { CheckValues } from "./CheckValues";
-import { EmptyTags } from "./EmptyTags";
-import { NewLineAfterDescription } from "./NewLineAfterDescription";
-import { NoTypes } from "./NoTypes";
-import { RequireDescription } from "./RequireDescription";
-import { RequireJSDoc } from "./RequireJSDoc";
-import { RequireParam } from "./RequireParam";
-import { RequireParamDescription } from "./RequireParamDescription";
-import { RequireParamType } from "./RequireParamType";
-import { RequireReturns } from "./RequireReturns";
-import { RequireReturnsType } from "./RequireReturnsType";
+import { CheckAlignment } from "./CheckAlignment.test";
+import { CheckIndentation } from "./CheckIndentation.test";
+import { CheckParamNames } from "./CheckParamNames.test";
+import { CheckSyntax } from "./CheckSyntax.test";
+import { CheckTagNames } from "./CheckTagNames.test";
+import { CheckValues } from "./CheckValues.test";
+import { EmptyTags } from "./EmptyTags.test";
+import { NewLineAfterDescription } from "./NewLineAfterDescription.test";
+import { NoTypes } from "./NoTypes.test";
+import { RequireDescription } from "./RequireDescription.test";
+import { RequireJSDoc } from "./RequireJSDoc.test";
+import { RequireParam } from "./RequireParam.test";
+import { RequireParamDescription } from "./RequireParamDescription.test";
+import { RequireParamType } from "./RequireParamType.test";
+import { RequireReturns } from "./RequireReturns.test";
+import { RequireReturnsType } from "./RequireReturnsType.test";
 
 export let JSDocTests = new ContainerSuite(
     "JSDoc",
