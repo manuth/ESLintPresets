cc6cc526355db34ae335842ace7dc32e78f7e09e
Manuel Thalmann <m@nuth.ch>
2021-07-25 12:39:53 +0200

    Update index-file to load presets only once

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { GenerateConfiguration as _GenerateESLintConfiguration } from "./Configu
 import { GenerateConfiguration as _GenerateTSLintConfiguration } from "./Configuration/TSLint/GenerateConfiguration";
 import { ESLintPlugin as _ESLintPlugin } from "./ESLintPlugin";
 import { ESLintRule as _ESLintRule } from "./ESLintRule";
-import { PresetName as _PresetName } from "./PresetName";
+import { PresetName, PresetName as _PresetName } from "./PresetName";
 import { TSLintRule as _TSLintRule } from "./TSLintRule";
 
 /**
@@ -46,25 +46,71 @@ namespace ESLintPresets
      */
     export let PluginName = "@manuth/typescript";
 
+    let cache: Map<PresetName, any> = new Map();
+
+    /**
+     * Loads the preset with the specified {@link presetName `presetName`}.
+     *
+     * @param presetName
+     * The name of the preset to load.
+     *
+     * @returns
+     * The preset with the specified {@link presetName `presetName`}.
+     */
+    function LoadPreset(presetName: PresetName): any
+    {
+        if (!cache.has(presetName))
+        {
+            let weak: boolean;
+            let typeChecking: boolean;
+
+            switch (presetName)
+            {
+                case PresetName.Recommended:
+                case PresetName.RecommendedWithTypeChecking:
+                    weak = false;
+                    break;
+                default:
+                    weak = true;
+                    break;
+            }
+
+            switch (presetName)
+            {
+                case PresetName.RecommendedWithTypeChecking:
+                case PresetName.WeakWithTypeChecking:
+                    typeChecking = true;
+                    break;
+                default:
+                    typeChecking = false;
+                    break;
+            }
+
+            cache.set(presetName, GenerateESLintConfiguration(weak, typeChecking));
+        }
+
+        return cache.get(presetName);
+    }
+
     /**
      * Provides configurations for `eslint`.
      */
     export let configs = {
         get [PresetName.Recommended]()
         {
-            return GenerateESLintConfiguration(false, false);
+            return LoadPreset(PresetName.Recommended);
         },
         get [PresetName.RecommendedWithTypeChecking]()
         {
-            return GenerateESLintConfiguration(false, true);
+            return LoadPreset(PresetName.RecommendedWithTypeChecking);
         },
         get [PresetName.Weak]()
         {
-            return GenerateESLintConfiguration(true, false);
+            return LoadPreset(PresetName.Weak);
         },
         get [PresetName.WeakWithTypeChecking]()
         {
-            return GenerateESLintConfiguration(true, true);
+            return LoadPreset(PresetName.WeakWithTypeChecking);
         }
     };
 }
